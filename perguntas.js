2c37e573ce2fc5f6acba175ae2666c97caf9b891
Hugo Souza Jamielniak <107001378+HugoSouza0611@users.noreply.github.com>
2024-09-23 10:59:56 +0000

    mjgjmhmkhg

--- perguntas.js
+++ perguntas.js
@@ -1,5 +1,20 @@
 criaCartao(
     'Química',
+    'O que é a Lei de Conservação da Massa e como ela se aplica nas reações químicas ?',
+    'A Lei de Conservação da Massa afirma que a massa total dos reagentes é igual à massa total dos produtos em uma reação química, ou seja, a massa não é criada nem destruída durante a reação.')
+
+criaCartao(
+    'Química',
+    'Qual é a principal diferença entre uma solução saturada e uma solução supersaturada ?',
+    'Uma solução saturada contém a máxima quantidade de soluto que pode ser dissolvida em um solvente a uma dada temperatura, enquanto uma solução supersaturada contém mais soluto do que o normalmente permitido a essa temperatura.')
+
+    criaCartao(
+        'Química',
+        'Oque é entalpia ?',
+        'É uma função termodinâmica pela qual se calcula o calor envolvido em processos isobáricos')
+
+criaCartao(
+    'Química',
     'Qual fórmula da concentração comum ?',
     'C = m1 / v.')
 
@@ -17,22 +32,7 @@ criaCartao(
     'Química',
     'Qual fórmula da densidade ?',
     'd = m / v.')
-
-criaCartao(
-    'Química',
-    'O que é a Lei de Conservação da Massa e como ela se aplica nas reações químicas ?',
-    'A Lei de Conservação da Massa afirma que a massa total dos reagentes é igual à massa total dos produtos em uma reação química, ou seja, a massa não é criada nem destruída durante a reação.')
-
-criaCartao(
-    'Química',
-    'Qual é a principal diferença entre uma solução saturada e uma solução supersaturada ?',
-    'Uma solução saturada contém a máxima quantidade de soluto que pode ser dissolvida em um solvente a uma dada temperatura, enquanto uma solução supersaturada contém mais soluto do que o normalmente permitido a essa temperatura.')
-
-criaCartao(
-    'Química',
-    'Oque é entalpia ?',
-    'É uma função termodinâmica pela qual se calcula o calor envolvido em processos isobáricos')
-
+        
 
 
 
